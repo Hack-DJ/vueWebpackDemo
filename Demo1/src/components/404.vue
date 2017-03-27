db45232fcbf403b32157ae868ca87a5ad14657d0
a305281839 <xianggege520@vip.qq.com>
2017-03-27 12:42:09 +0800

    初始化DEMO

---
+++ Demo1/src/components/404.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="error">
+		{{msg}}
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'error',
+		data() {
+			return {
+				msg: "错误页"
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ Demo1/src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="test">
+		{{msg}}
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'test',
+		data() {
+			return {
+				msg: "你好啊"
+			}
+		},
+		mounted:function(){
+			mui.toast(123);
+		}
+	}
+</script>
+
+<style>
+
+</style>
